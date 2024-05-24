eabc01b370d547369145b51ea1dedbd36914aae0
sigzal1105 <sigzal1105@skola.goteborg.se>
2024-05-24 23:44:19 +0200

    Added post and get methods to BookController

--- src/main/java/se/yrgo/library/controllers/BookController.java
+++ src/main/java/se/yrgo/library/controllers/BookController.java
@@ -1,14 +1,40 @@
 package se.yrgo.library.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Book;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("website/library")
 public class BookController {
+    @Autowired
+    private BookRepository data;
 
+    //Adds new book to the db.
+    @RequestMapping(value = "/new-book", method = RequestMethod.POST)
+    public Book newBook(@RequestBody Book book) {
+        data.save(book);
+        return book;
+    }
+
+    //An empty book for the user to fill in.
+    @RequestMapping(value = "/new-book", method = RequestMethod.GET)
+    public Book renderNewBook() {
+        return new Book();
+    }
+
+    //Lists all books.
+    @RequestMapping(value = "/list-books", method = RequestMethod.GET)
+    public List<Book> books() {
+        return data.findAll();
+    }
+
+    //Get book by name.
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public Book getBookByName(@PathVariable("name") String name) {
+        return data.findByName(name);
+    }
 }
