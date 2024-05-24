5029a6fdc28c0fe97b0e7633756b150b878f727b
sigzal1105 <sigzal1105@skola.goteborg.se>
2024-05-24 23:57:37 +0200

    removed uneccessary method

--- src/main/java/se/yrgo/library/controllers/BookController.java
+++ src/main/java/se/yrgo/library/controllers/BookController.java
@@ -3,7 +3,6 @@ package se.yrgo.library.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 import java.util.List;
 
 
@@ -18,12 +17,6 @@ public class BookController {
     public Book newBook(@RequestBody Book book) {
         data.save(book);
         return book;
-    }
-
-    //An empty book for the user to fill in.
-    @RequestMapping(value = "/new-book", method = RequestMethod.GET)
-    public Book renderNewBook() {
-        return new Book();
     }
 
     //Lists all books.
